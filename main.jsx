a60cea0e3b94ea758c0a81fdd0a75ca870d864be
Giorgio Bozio <giorgio.bozio@gmail.com>
2014-10-24 00:33:56 +0200

    static component, almost there

--- main.jsx
+++ main.jsx
@@ -1,7 +1,14 @@
 var React = require('react')
   , Paginator = require('./paginator.jsx')
-
-  var component = Paginator()
+  , rows = [
+              {name: 'Giorgio', surname: 'Bozio'},
+              {name: 'Mario', surname: 'Rossi'},
+              {name: 'Giorgio', surname: 'Bozio'},
+              {name: 'Giorgio', surname: 'Bozio'},
+              {name: 'Giorgio', surname: 'Bozio'},
+              {name: 'Giorgio', surname: 'Bozio'},
+            ]
+var component = Paginator({rows: rows, itemsPerPage: '5'})
   
 React.renderComponent(
   component,
--- paginator.jsx
+++ paginator.jsx
@@ -1,9 +1,39 @@
 var React = require('react')
+  , _ = require('underscore')
 
-var Paginator = React.createClass({
+var List = React.createClass({
+  render: function(){
+    var createRow = function(row){
+        return <li>{row.name} {row.surname}</li>
+      }
+    return <ul>{this.props.rows.map(createRow)}</ul>;
+  }
+})
+
+var PageLinks = React.createClass({
   render: function(){
     return (
-      <h1>Ciao!</h1>
+      <div><a href="" onClick={this.props.onNextPage}>next page</a></div>
+    )
+  }
+})
+
+var Paginator = React.createClass({
+  getInitialState: function() {
+    return {currentPage: 1}
+  },
+  onNextPage: function(e) {
+    e.preventDefault()
+    console.log(e)
+  },
+  render: function(){
+    var currentRows = this.props.rows.slice(0, this.props.itemsPerPage)
+    return (
+      <div>
+        <h1>Ciao!</h1>
+        <List rows={currentRows} />
+        <PageLinks onNextPage={this.onNextPage} />
+      </div>
     )
   }
 })
